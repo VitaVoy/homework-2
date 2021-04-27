cbacace7408bb6f173c0fc36d1e662739175c354
Vitali Voitau <videofag@mail.ru>
2021-04-27 21:18:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h;
+            double m;
+            Console.WriteLine("Введите свой рост в метрах");
+            string str = Console.ReadLine();
+            h = Convert.ToDouble(str);
+            Console.WriteLine("Введите свой вес в килограммах");
+            m = Convert.ToDouble(Console.ReadLine());
+            double i = m / (h + h);
+            Console.WriteLine(i);
+        }
+    }
+}
